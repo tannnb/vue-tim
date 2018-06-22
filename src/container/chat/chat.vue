8fd388ec36c2c397300dca1489c0d553e81b2982
tannnb <tanbingwork@163.com>
2018-06-22 22:32:31 +0800

    聊天页

---
+++ src/container/chat/chat.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>用户名：{{this.$store.state.users.initState.user}}</div>
+    <div>{{chatlist}}</div>
+  </div>
+</template>
+
+<script>
+  import io from 'socket.io-client'
+
+  export default {
+    name: "chat",
+    data() {
+      return {
+        chatlist: ''
+      }
+    },
+    created(){
+      const socket = io('ws://localhost:9093')
+      socket.on('obj1',  (data) => {
+        if(!data){
+          return false
+        }
+        this.chatlist = data.hello
+      })
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
